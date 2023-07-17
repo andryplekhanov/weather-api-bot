2f9b5851674db0b66a1e708ad3c3dca0b190a5e8
Andrey Plekhanov <1989plekhanov@gmail.com>
2023-07-17 18:46:14 +0300

    add user to db with /start

--- tgbot/handlers/user.py
+++ tgbot/handlers/user.py
@@ -2,11 +2,13 @@ from aiogram import Dispatcher
 from aiogram.dispatcher import FSMContext
 from aiogram.types import Message
 
+from tgbot.models import orm
 from tgbot.services.set_bot_commands import get_default_commands
 
 
 async def user_start(message: Message, state: FSMContext):
     await state.finish()
+    await orm.add_user(message.bot.get('db'), message.from_user.id)
     text = f'Привет, {message.from_user.username}! Я бот, который расскажет о погоде на сегодня.'
     await message.answer(text)
     commands = await get_default_commands()
