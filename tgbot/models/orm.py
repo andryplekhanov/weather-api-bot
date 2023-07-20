d66fa413e572a1e4d7661242f9773fbbe0ba8384
Andrey Plekhanov <1989plekhanov@gmail.com>
2023-07-20 15:23:01 +0300

    add logging

--- tgbot/models/orm.py
+++ tgbot/models/orm.py
@@ -1,17 +1,23 @@
+import logging
+
 from sqlalchemy import select
 
 from tgbot.models.models import User
+
+logger = logging.getLogger(__name__)
 
 
 async def add_user(async_session, user_id):
     async with async_session() as session:
         user = await session.execute(select(User).where(User.tg_id == user_id))
         all_users = user.scalars().all()
+        # logger.info(f"Users: {all_users}")
 
         if str(user_id) not in list(map(str, all_users)):
             new_user = User(tg_id=user_id)
             session.add(new_user)
             await session.commit()
+            logger.info(f"User '{user_id}' was added to DB")
 
 
 async def set_user_city(async_session, user_id, city_name, city_geo):
@@ -22,6 +28,7 @@ async def set_user_city(async_session, user_id, city_name, city_geo):
         user.city_name = city_name
         user.city_geo = city_geo
         await session.commit()
+        logger.info(f"City added for User '{user.tg_id}' == {user.city_name}")
 
 
 async def get_users_city(async_session, user_id):
